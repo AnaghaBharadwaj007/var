6a4bd7ef6bb5385b9c7c2897dda4840b12023081
Anagha Bharadwaj <155178921+AnaghaBharadwaj007@users.noreply.github.com>
2026-05-01 19:10:49 +0530

    a1

--- script.js
+++ script.js
@@ -842,3 +842,12 @@ function init() {
 }
 
 init();
+
+// Register Service Worker for PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(() => console.log('SW registered'))
+      .catch(err => console.log('SW error', err));
+  });
+}
